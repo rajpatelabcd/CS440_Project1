7622cf380cd0561d7398a8c48fdab5414a305f52
raj patel <rajpatelpiplav@gmail.com>
2026-09-20 19:11:34 -0400

    a start

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello Yug")
+import random
+
+
+
+GRID_SIZE = 10;
+
+grid = [['*' for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
+
+
+def get_neighbors((x, y)):
+    return {
+        "left": (x, y - 1),
+        "right": (x, y + 1),
+        "top": (x - 1, y),
+        "bottom": (x + 1, y)
+    }
+
+open_cell = (random.randint(1, GRID_SIZE - 1), random.randint(1, GRID_SIZE - 1))
+row, column = open_cell
+grid[row][column] = '$' 
+
+ 
+
+print(open_cell)
+
+for i in range(GRID_SIZE):
+    print(grid[i])
