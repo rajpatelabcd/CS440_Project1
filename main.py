315ca6b5898eb143944af5a5c669797137a154aa
raj patel <rajpatelpiplav@gmail.com>
2026-09-21 10:17:24 -0400

    some working code

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import random
 GRID_SIZE = 5;
 grid = [['*' for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
 
-def get_neighbors((x, y)):
-    if((x > 0) and (x < GRID_SIZE - 1) and (y > 0) and (y < GRID_SIZE - 1)):
-        return {
-            "left": (x, y - 1),
-            "right": (x, y + 1),
-            "top": (x - 1, y),
-            "bottom": (x + 1, y)
-        }
-    else:
-        return 0
+def get_neighbors(pos):
+    x, y = pos
+    neighbors = {}
+
+    if x > 0: neighbors["top"] = (x - 1, y)
+    if x < GRID_SIZE - 1: neighbors["bottom"] = (x + 1, y)
+    if y > 0: neighbors["left"] = (x, y - 1)
+    if y < GRID_SIZE - 1: neighbors["right"] = (x, y + 1)
+
+    return neighbors
 
 # the first cell to open
 open_cell = (random.randint(1, GRID_SIZE - 1), random.randint(1, GRID_SIZE - 1))
@@ -24,7 +24,7 @@ grid[row][column] = '.'
 valid_cells = []    
 neighbors = get_neighbors((row, column))
 
-if(neighbors != 0):
+if neighbors:
     valid_cells = list(neighbors.values())
 
 # look through each valid cell and randomlly open cell 
@@ -33,24 +33,22 @@ while valid_cells:
     valid_cell_to_open = valid_cells.pop(rand)
     valid_cell_row, valid_cell_column = valid_cell_to_open
 
-    neighbors = get_neighbors((valid_cell_row, valid_cell_column))
+    neighbors = get_neighbors(valid_cell_to_open)
 
     if((1 < valid_cell_row < GRID_SIZE - 1) and (1 < valid_cell_column < GRID_SIZE - 1)):
         grid[valid_cell_row][valid_cell_column] = '.'
   
     is_valid = all(
-    grid[row][col] == "*" 
-    for row, col in neighbors.values() 
-    if (row, col) != (valid_cell_row, valid_cell_column)  
-    and 0 <= row < GRID_SIZE                         
-    and 0 <= col < GRID_SIZE                         
+        grid[row][col] == "*" 
+        for row, col in neighbors.values() 
+        if (row, col) != (valid_cell_row, valid_cell_column)                          
     )
     neighbor_list = list(neighbors.values())
 
     for n in neighbors.values():
-        if ((n != neighbor_list[rand]) and is_valid):
-            raj, patel = n 
-            valid_cells.append((raj, patel))
+        if rand < len(neighbor_list) and n != neighbor_list[rand] and is_valid:
+            valid_cells.append(n)
+
 
     print(valid_cells)
 
