ce904708c695d895aafa6dd18eefdd0df76aa862
raj patel <rajpatelpiplav@gmail.com>
2026-09-21 12:23:30 -0400

    maze part one working

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 
-GRID_SIZE = 5;
+GRID_SIZE = 5
+
 grid = [['*' for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
 
 def get_neighbors(pos):
@@ -15,42 +16,49 @@ def get_neighbors(pos):
     return neighbors
 
 # the first cell to open
-open_cell = (random.randint(1, GRID_SIZE - 1), random.randint(1, GRID_SIZE - 1))
+open_cell = (random.randint(1, GRID_SIZE - 2), random.randint(1, GRID_SIZE - 2))
 row, column = open_cell
 grid[row][column] = '.' 
+last_carved_cell = open_cell
 
 # list of all cells which are valid and we can open (with exectly one neighbor)
 
 valid_cells = []    
-neighbors = get_neighbors((row, column))
+initial_neighbors = get_neighbors((row, column))
 
-if neighbors:
-    valid_cells = list(neighbors.values())
+if initial_neighbors:
+    valid_cells = list(initial_neighbors.values())
+
+print('initial valid cells are: ', valid_cells)
+print('\n\n')
 
 # look through each valid cell and randomlly open cell 
 while valid_cells:
     rand = random.randint(0, len(valid_cells) - 1)
     valid_cell_to_open = valid_cells.pop(rand)
+
+    print('random cells we picked to open: ', valid_cell_to_open)
+    print('\n\n')
+
     valid_cell_row, valid_cell_column = valid_cell_to_open
 
     neighbors = get_neighbors(valid_cell_to_open)
 
-    if((1 < valid_cell_row < GRID_SIZE - 1) and (1 < valid_cell_column < GRID_SIZE - 1)):
+    if((0 < valid_cell_row < GRID_SIZE) and (0 < valid_cell_column < GRID_SIZE)):
         grid[valid_cell_row][valid_cell_column] = '.'
+        last_carved_cell = valid_cell_to_open
   
-    is_valid = all(
-        grid[row][col] == "*" 
-        for row, col in neighbors.values() 
-        if (row, col) != (valid_cell_row, valid_cell_column)                          
-    )
+    opened_neighbors = sum(1 for r, c in neighbors.values() if grid[r][c] == '.')
+
+    is_valid = True if (opened_neighbors == 1) else False
+
     neighbor_list = list(neighbors.values())
 
     for n in neighbors.values():
-        if rand < len(neighbor_list) and n != neighbor_list[rand] and is_valid:
+        if (rand < len(neighbor_list) and n != neighbor_list[rand]) and (is_valid):
             valid_cells.append(n)
-
-
-    print(valid_cells)
+    print('the list after adding the valid neighbor: ', valid_cells)
+    print('\n\n')
 
 for i in range(GRID_SIZE):
     print(grid[i])
