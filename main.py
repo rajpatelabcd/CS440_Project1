8d9c80e520ab09456d67e64204eeec59d5e32c22
raj patel <rajpatelpiplav@gmail.com>
2026-09-21 16:03:07 -0400

    some imp changes

--- main.py
+++ main.py
@@ -19,15 +19,12 @@ def get_neighbors(pos):
 open_cell = (random.randint(1, GRID_SIZE - 2), random.randint(1, GRID_SIZE - 2))
 row, column = open_cell
 grid[row][column] = '.' 
-last_carved_cell = open_cell
 
 # list of all cells which are valid and we can open (with exectly one neighbor)
 
-valid_cells = []    
-initial_neighbors = get_neighbors((row, column))
-
-if initial_neighbors:
-    valid_cells = list(initial_neighbors.values())
+valid_cells = []
+for neighbor in get_neighbors(open_cell).values():
+    valid_cells.append(neighbor)
 
 print('initial valid cells are: ', valid_cells)
 print('\n\n')
@@ -36,29 +33,27 @@ print('\n\n')
 while valid_cells:
     rand = random.randint(0, len(valid_cells) - 1)
     valid_cell_to_open = valid_cells.pop(rand)
-
-    print('random cells we picked to open: ', valid_cell_to_open)
-    print('\n\n')
-
     valid_cell_row, valid_cell_column = valid_cell_to_open
 
-    neighbors = get_neighbors(valid_cell_to_open)
+    print('random cells we picked to open: ', valid_cell_to_open)
+    if grid[row][column] == '.':
+        continue
+    print('\n\n')
 
-    if((0 < valid_cell_row < GRID_SIZE) and (0 < valid_cell_column < GRID_SIZE)):
-        grid[valid_cell_row][valid_cell_column] = '.'
-        last_carved_cell = valid_cell_to_open
-  
-    opened_neighbors = sum(1 for r, c in neighbors.values() if grid[r][c] == '.')
+    neighbors = get_neighbors(cell)
 
-    is_valid = True if (opened_neighbors == 1) else False
+    opened_neighbors = sum( 1 for r, c in neighbors.values() if grid[r][c] == '.')
+    if opened_neighbors != 1:
+        continue
+    grid[row][column] = '.'
 
-    neighbor_list = list(neighbors.values())
 
     for n in neighbors.values():
-        if (rand < len(neighbor_list) and n != neighbor_list[rand]) and (is_valid):
+        r, c = n
+        if grid[r][c] == '*' and n not in valid_cells:
             valid_cells.append(n)
     print('the list after adding the valid neighbor: ', valid_cells)
     print('\n\n')
 
-for i in range(GRID_SIZE):
-    print(grid[i])
+for row in grid:
+    print(row)
